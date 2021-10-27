9cf087909d96a9936d5eae9dc8bc7112eb2bcce0
adambeilchick <apbeilchick@gmail.com>
2021-10-27 10:17:56 -0400

    Fixed dashboard route to use DashboardComponent

--- src/app/components/resources/resources-routing.module.ts
+++ src/app/components/resources/resources-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { PatientComponent, ResourcesTableContainerComponent, EditResourceComponent } from '.';
+import { PatientComponent, ResourcesTableContainerComponent, EditResourceComponent, DashboardComponent } from '.';
 import { AuthGaurd } from '../../misc/auth-guard';
 
 const routes: Routes = [
   { path: 'resources/Patient', component: PatientComponent, canActivate: [AuthGaurd] },
-  { path: 'resources/dashboard', component: PatientComponent, canActivate: [AuthGaurd] },
+  { path: 'resources/dashboard', component: DashboardComponent, canActivate: [AuthGaurd] },
   { path: 'resources/:resourceType', component: ResourcesTableContainerComponent, canActivate: [AuthGaurd] },
   { path: 'resources/:resourceType/:id', component: EditResourceComponent, canActivate: [AuthGaurd] },
 ];
