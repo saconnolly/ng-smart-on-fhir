9c429bee35f48633bd9c2e6d906d012621feb041
shaun connolly <shaun.connolly@philips.com>
2021-10-27 14:20:39 -0400

    Cleanup some styling, add components, fix patient demographics

--- src/app/components/resources/index.ts
+++ src/app/components/resources/index.ts
@@ -4,3 +4,5 @@ export * from './resources-table/resources-table.component';
 export * from './resources-table-container/resources-table-container.component';
 export * from './dashboard/dashboard.component';
 export * from './patient-demographics/patient-demographics.component';
+export * from './patterns-of-use/patterns-of-use.component';
+export * from './therapy-data/therapy-data.component';
---
+++ src/app/components/resources/therapy-data/therapy-data.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-therapy-data',
+  templateUrl: './therapy-data.component.html',
+  styleUrls: ['./therapy-data.component.css']
+})
+export class TherapyDataComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+}
